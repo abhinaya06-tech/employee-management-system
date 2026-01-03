91f4e944da69d5b7c159cca583e638897418c5de
abhinaya06-tech <j.abhinaya12@gmail.com>
2026-01-03 18:47:00 +0530

    Initial Spring Boot setup with test API

---
+++ src/main/java/com/example/employeemanagement/EmployeeManagementSystemApplication.java
@@ -0,0 +1,18 @@
+package com.example.employeemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(
+        excludeName = {
+                "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration",
+                "org.springframework.boot.autoconfigure.jdbc.DataSourceInitializationAutoConfiguration",
+                "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
+        }
+)
+public class EmployeeManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeManagementSystemApplication.class, args);
+    }
+}
